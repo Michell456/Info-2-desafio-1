2f45ef5e29f9502dd6522d7c032fecb85efdccae
manueljmontoya <manuelj.montoya@udea.edu.co>
2025-09-23 20:49:38 -0500

    Creación de función de descomprimidor LZ78 y desencriptador

--- Desafio1/main.cpp
+++ Desafio1/main.cpp
@@ -51,9 +51,79 @@ unsigned char* descomprimidorRLE(unsigned char* msj,int tamanoMsj){
     return resultadoMsj;
 }
 
+unsigned char* descompresorLZ78(unsigned char msj[],int tamanoArchivo){
 
+    int cant_pares=tamanoArchivo/3;
+    int *tamanos= new int[cant_pares];
+    int longitud=0, total =0;
 
+    int j=0, i=0;
+    while (j<tamanoArchivo){
 
-int main() {
-    return 0;
+        unsigned char byte1=msj[j], byte2=msj[j+1];
+        int index=(byte1<<8)|byte2;
+        j+=3;
+
+        if (index==0){
+            longitud=1;
+        }
+        else{
+            longitud=tamanos[index-1]+1;
+        }
+
+        total+=longitud;
+        tamanos[i]=longitud;
+        i++;
+    }
+
+    unsigned char *descomprimido= new unsigned char[total];
+    int *posiciones= new int[cant_pares];
+
+    j=0, i=0;
+    int entradas=0;
+
+    while (j<tamanoArchivo){
+
+        unsigned char byte1=msj[j], byte2=msj[j+1], caracter_actual=msj[j+2];
+        int index=(byte1<<8)|byte2;
+        j+=3;
+
+        if (index==0){
+            descomprimido[entradas]=caracter_actual;
+            posiciones[i]=entradas;
+            entradas++;
+            i++;
+        }
+        else{
+            int entrada_inicial=entradas;
+            for (int c=0; c<tamanos[index-1];c++){
+                descomprimido[entradas]=descomprimido[posiciones[index-1]+c];
+                entradas++;
+            }
+            descomprimido[entradas]=caracter_actual;
+            entradas++;
+            posiciones[i]=entrada_inicial;
+            i++;
+        }
+    }
+
+    delete[] tamanos;
+    delete[] posiciones;
+
+    return descomprimido;
 }
+
+
+unsigned char* desencriptador(int n,unsigned char key,unsigned char msj[],int tamanoArchivo){
+
+    unsigned char *desencriptado= new unsigned char[tamanoArchivo];
+
+    for (int j=0; j<tamanoArchivo; j++){
+
+        unsigned char byte_result = msj[j] ^ key;
+        desencriptado[j]=(byte_result>>n)|(byte_result<<(8-n));
+    }
+
+    return desencriptado;
+}
+
